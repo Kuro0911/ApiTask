3c7875511ae8c482af075872bec825ee1d00a70e
Dhananjai <dhananjaisharma09@gmail.com>
2022-01-22 19:49:57 +0530

    fixes

--- tables/src/Home.js
+++ tables/src/Home.js
@@ -5,13 +5,7 @@ function Home() {
     <div className="banner">
       <div className="banner-container">
         <div className="banner-txt">Tables</div>
-        <button className="banner-btns">
-          <img
-            className="home-btn"
-            src="https://lh3.googleusercontent.com/aypmZSvaY7OmCJKc9WpgtF86lcsIzhdRJLRDgoxVVSNOjqlXE70pmJeW-YpKysalY1hFVVK1VO7jFywgK8J-_8360AI--fg-A62uK3e_nr-PKGQGTNa7Okc71ycAeGMBF5VQoz7fsA=w237-h207-p-k"
-          />
-          / Tables / Tables
-        </button>
+        <button className="banner-btns">Tables / tables</button>
         <div className="btn-container">
           <button className="banner-btn">New</button>
           <button className="banner-btn">Filters</button>
