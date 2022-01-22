0f30e3e7da830c52cc778b29cd3bc6dc94c5a360
Dhananjai <dhananjaisharma09@gmail.com>
2022-01-22 19:47:15 +0530

    fixes

--- tables/src/Home.js
+++ tables/src/Home.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import './Home.css'
+import React from "react";
+import "./Home.css";
 function Home() {
   return (
-        <div className="banner">
-            <div className='banner-container'>
-                <div className='banner-txt'>Tables</div>
-                <button className='banner-btns'>Tables / Tables</button>
-                <div className='btn-container'>
-                    <button className='banner-btn'>New</button>
-                    <button className='banner-btn'>filters</button>
-                </div>
-            </div>
+    <div className="banner">
+      <div className="banner-container">
+        <div className="banner-txt">Tables</div>
+        <button className="banner-btns">
+          <img
+            className="home-btn"
+            src="https://lh3.googleusercontent.com/aypmZSvaY7OmCJKc9WpgtF86lcsIzhdRJLRDgoxVVSNOjqlXE70pmJeW-YpKysalY1hFVVK1VO7jFywgK8J-_8360AI--fg-A62uK3e_nr-PKGQGTNa7Okc71ycAeGMBF5VQoz7fsA=w237-h207-p-k"
+          />
+          / Tables / Tables
+        </button>
+        <div className="btn-container">
+          <button className="banner-btn">New</button>
+          <button className="banner-btn">Filters</button>
         </div>
-  )
+      </div>
+    </div>
+  );
 }
 
 export default Home;
--- tables/src/NavBar.js
+++ tables/src/NavBar.js
@@ -1,24 +1,28 @@
-import React from 'react'
-import './Navbar.css'
+import React from "react";
+import "./Navbar.css";
 
 function NavBar() {
-    const handleClick1 = () => {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    };
-    const handleClick2 = () => {
-        window.open("https://github.com/Kuro0911/ApiTask");
-    };
-    return (
-        <div className={`nav`}>
-            <div className='search-bar' onClick={() => handleClick1()}/>
-            <img 
-                className="nav-profile"
-                onClick={() => handleClick2()}
-                src="https://lh3.googleusercontent.com/Os0eg1CYwpqlkTovCTVEI0SuBFH0adgc2hWx9_0Bm5wgosjD0nqTMx0WOPUI0kg4tjvAx-SP2SgCDbRBftjdNbXFGbqd3-fIJbAxKYTVtK3BmOQFYJIlrQWOUH3M3ezmQ8iDiSR5CQ=s120-p-k"
-                alt="profile"
-            />            
-          </div>
-    )
+  const handleClick2 = () => {
+    window.open("https://github.com/Kuro0911/ApiTask");
+  };
+  return (
+    <div className={`nav`}>
+      <div class="search-container">
+        <form action="">
+          <input type="text" placeholder="Search..." name="search"></input>
+          <button type="submit">
+            <i class="fa fa-search"></i>
+          </button>
+        </form>
+      </div>
+      <img
+        className="nav-profile"
+        onClick={() => handleClick2()}
+        src="https://lh3.googleusercontent.com/Os0eg1CYwpqlkTovCTVEI0SuBFH0adgc2hWx9_0Bm5wgosjD0nqTMx0WOPUI0kg4tjvAx-SP2SgCDbRBftjdNbXFGbqd3-fIJbAxKYTVtK3BmOQFYJIlrQWOUH3M3ezmQ8iDiSR5CQ=s120-p-k"
+        alt="profile"
+      />
+    </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
