679d23c9d39ee3b01966bbc6761d5109d7ad2930
Dhananjai <dhananjaisharma09@gmail.com>
2022-01-22 10:35:55 +0530

    fixes

--- tables/src/NavBar.js
+++ tables/src/NavBar.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React from 'react'
 import './Navbar.css'
 
-
 function NavBar() {
-  return <div className='navbar'>
-      ola
-  </div>;
+    const handleClick1 = () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+    const handleClick2 = () => {
+        window.open("https://github.com/Kuro0911/ApiTask");
+    };
+    return (
+        <div className={`nav`}>
+            <div className='search-bar' onClick={() => handleClick1()}/>
+            <img 
+                className="nav-profile"
+                onClick={() => handleClick2()}
+                src="https://lh3.googleusercontent.com/Os0eg1CYwpqlkTovCTVEI0SuBFH0adgc2hWx9_0Bm5wgosjD0nqTMx0WOPUI0kg4tjvAx-SP2SgCDbRBftjdNbXFGbqd3-fIJbAxKYTVtK3BmOQFYJIlrQWOUH3M3ezmQ8iDiSR5CQ=s120-p-k"
+                alt="profile"
+            />            
+          </div>
+    )
 }
 
-export default NavBar;
-
+export default NavBar
